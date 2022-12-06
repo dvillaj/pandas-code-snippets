8aebf83c861cd2ebaf7f5f2f02020a109959948f
Daniel Villanueva Jimenez <dvillaj@gmail.com>
2022-12-06 10:34:19 +0100

    exception in clean snippets

--- utils/clean-snippets.py
+++ utils/clean-snippets.py
@@ -7,8 +7,12 @@ logger = logging.getLogger("app")
 
 logger.info("Beginning ...")
 for file in glob.glob("*.json"):
-    snippet = read_file(file)
-    logger.info(f"Cleaning '{get_title(snippet)} ...")    
-    clean_snippet(file, snippet)
+    try:
+        snippet = read_file(file)
+        logger.info(f"Cleaning '{get_title(snippet)} ...")    
+        clean_snippet(file, snippet)
+    except:
+        raise Exception(f"Sorry, An exception occurred in file '{file}'")
+    
 
 logger.info("All good :-)")
